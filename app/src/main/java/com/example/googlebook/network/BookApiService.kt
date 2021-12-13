b9ac72a8d0667e05544075bc6ba7ad55f3e89ea6
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2021-12-13 09:21:23 +0300

    initial commit

---
+++ app/src/main/java/com/example/googlebook/network/BookApiService.kt
@@ -0,0 +1,34 @@
+package com.example.googlebook.network
+
+import com.example.googlebook.data.BooksData
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://www.googleapis.com"
+private const val ENDPOINT = "/books/v1/volumes"
+private const val DEFAULT = "?q=search+terms"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface BookApiService{
+    @GET(ENDPOINT+"AIzaSyBlkY49AJ-dhEZCULByKv5Gh1C62WPicCE")
+    suspend fun getBook(@Query("q") q : String = DEFAULT) : BooksData
+}
+
+object MoviesApi {
+    val retrofitService: BookApiService by lazy {
+        retrofit.create(BookApiService::class.java)
+    }
+}
